5cba4651f59693bc1663a08ea742f991a4562d12
DevJeipi <joaopedroghilardi@gmail.com.br>
2026-03-05 13:34:03 -0300

    raw: header gaps and hero description left gap

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -74,7 +74,7 @@ export default function Hero() {
         </h1>
 
         {/* Description Text */}
-        <div className="absolute bottom-24 md:bottom-8 left-4 md:left-0 z-10 max-w-[90%] md:max-w-md">
+        <div className="absolute bottom-24 md:bottom-8 left-4 md:left-6 z-10 max-w-[90%] md:max-w-md">
           <p
             className="text-sm md:text-base text-justify relative indent-10 
                        before:content-[''] before:absolute before:left-0 before:top-3
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,8 +5,8 @@ import { ThemeToggle } from "@/components/ui/ThemeToggle";
 
 export function Header() {
   return (
-    <header className="fixed container shadow-2xl rounded-4xl top-2 z-[1100] w-full bg-white/30 backdrop-blur-sm transition-colors">
-      <div className="flex rounded-4xl items-center justify-between h-16 sm:h-20 px-4">
+    <header className="fixed left-0 right-0 mx-6 sm:mx-8 shadow-2xl rounded-4xl top-2 z-[1100] bg-white/30 backdrop-blur-sm transition-colors">
+      <div className="flex rounded-4xl items-center justify-between h-16 sm:h-20 px-8">
         <Link href="/">
           <Image
             src="/logo-conecta-wtbg-blue-logotext.webp"
