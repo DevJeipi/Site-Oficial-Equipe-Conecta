1b982174c81e868404f9e0a444a35cfcfeb5b79c
DevJeipi <joaopedroghilardi@gmail.com>
2026-04-15 15:41:08 -0300

    pixel replaced

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -131,11 +131,19 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '3842552005964155');
+fbq('init', '497075456150333');
 fbq('track', 'PageView');
             `,
           }}
         />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=497075456150333&ev=PageView&noscript=1"
+          />
+        </noscript>
         <ThemeProvider attribute="class" enableSystem defaultTheme="system">
           {children}
         </ThemeProvider>
